c147966df15900a77b0326395b21db62dfd89fe5
Nate Wildermuth <nate.wildermuth@gmail.com>
2013-09-18 22:31:49 -0500

    Init with files

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularAppsApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
